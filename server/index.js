fda54c0742f5bb140a8e7834cd796d5ed302208d
harikesh <138229790+harik3shhh@users.noreply.github.com>
2024-05-14 00:22:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const port = process.env.PORT;
 
 const corsOptions = {
-    origin: ["*", "https://leave-notice-harikesh.vercel.app/"],
+    origin: ["*", "https://leave-notice-harikesh.vercel.app/register"],
     methods: "GET, POST, PUT, DELETE, HEAD",
     credentials: true,
 };
